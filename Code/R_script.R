0f8574f74f2a881f4fc515ea86c6b9251988fd4d
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-02-26 15:49:22 +0100

    Create R_script.R

---
+++ Code/R_script.R
@@ -0,0 +1,17 @@
+#First code
+
+sonia<-c(
+giorgia<-(
+  
+plot(giorgia, sonia, col="blue", pch=19, cex=2, xlab="pollution", ylab="numero di delfini")
+
+#installaione pacchetti
+#CRAN
+install.packages("terra")
+library(terra)
+
+#GitHub
+install.packages("devtools")  #remotes
+library(devtools)
+install_github("ducciorocchini/imageRy")
+  
