60dd29bc9c0056dc754ee0357fc9529edd326d14
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-03-04 17:33:06 +0100

    Update 02_visualization.R

--- Code/02_visualization.R
+++ Code/02_visualization.R
@@ -3,6 +3,12 @@
 library(terra)
 library(imageRy)
 
+install.packages("viridis")
+library(viridis)
+
+#library(devtools)
+#install_github("")
+
 #listing data
 im.list()
 
@@ -15,6 +21,26 @@ plot(b2, col=cl)
 
 cl <- colorRampPalette(c("darkolivegreen", "brown1", "gold"))(3)
 plot(b2, col=cl)
+
+#viridis palette
+plot(b2, col=inferno(100))
+
+#exercise: plot with mako
+plot(b2, col=mako(100))
+plot(b2, col=mako(3)) #brutto
+
+#plot b2 in grey scale
+cl <- colorRampPalette(c("darkgrey", "grey", "lightgrey"))(100)
+plot(b2, col=cl)
+
+
+
+
+
+
+
+
+
 
 
 
