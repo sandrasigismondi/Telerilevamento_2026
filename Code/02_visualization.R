dff6b509aad20e3a08c94c015b4c147a8663bdbe
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-03-05 15:02:43 +0100

    Update 02_visualization.R

--- Code/02_visualization.R
+++ Code/02_visualization.R
@@ -44,12 +44,31 @@ plot(b2, col=cl)
 plot(b2, col=inferno(100))
 
 
+##5/03
+####andrea
+#esercizio fare 4 plot multiframe per ogni banda                 
+                 
+par(mfrow=c(2,2))
+cl=colorRampPalette(c("lightblue","blue","darkblue"))(100)
+plot(b2,col=cl)
+cl=colorRampPalette(c("lightgreen","green","darkgreen"))(100)
+plot(b3,col=cl)
+cl=colorRampPalette(c("red","red2","red4"))(100)
+plot(b4,col=cl)
+cl=colorRampPalette(c("orange","magenta","red"))(100)
+plot(b8,col=cl)
+
+
+
 #######
 clb<-colorRampPalette(c("dark blue", "blue", "light blue"))(100)
 plot(b2, col=clb)
 
 clg<-colorRampPalette(c("dark green", "green", "light green"))(100)
 plot(b3, col=clg)
+
+clr<-colorRampPalette(c("#8B1A1A", "red", "pink"))(100)
+plot(b4, col=clg)
 
 cln<-colorRampPalette(c("goldenrod3", "goldenrod2", "goldenrod"))(100)
 plot(b8, col=cln)
@@ -58,3 +77,12 @@ plot(b2, col=inferno(100))
 plot(b3, col=inferno(100))
 plot(b4, col=inferno(100))
 plot(b8, col=inferno(100))
+
+
+
+
+
+
+
+
+
