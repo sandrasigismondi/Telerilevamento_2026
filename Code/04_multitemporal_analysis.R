c4583cfeac0ad3e0680472b4fe401446902abb4f
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-03-19 15:06:05 +0100

    Update 04_multitemporal_analysis.R

--- Code/04_multitemporal_analysis.R
+++ Code/04_multitemporal_analysis.R
@@ -9,10 +9,18 @@ EN_01 <- im.import("EN_01.png")
 EN_01 <- flip(EN_01)
 plot(EN_01)
 
+EN_13 <- im.import("EN_13.png")
+EN_13 <- flip(EN_13)
+plot(EN_13)
 
+im.multiframe(2,1)
+plot(EN_01)
+plot(EN_13)
 
-
-
+# differencing
+ENdif <- EN_01[[1]] - EN_13[[1]]
+dev.off()
+plot(ENdif)
 
 
 
