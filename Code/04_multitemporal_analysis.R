8d099e92d67da858318f968ceef03cf885afee91
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-03-19 15:40:42 +0100

    Update 04_multitemporal_analysis.R

--- Code/04_multitemporal_analysis.R
+++ Code/04_multitemporal_analysis.R
@@ -2,6 +2,7 @@
 
 library(terra)
 library(imageRy)
+library(viridis)
 
 im.list()
 
@@ -21,6 +22,48 @@ plot(EN_13)
 ENdif <- EN_01[[1]] - EN_13[[1]]
 dev.off()
 plot(ENdif)
+
+# greenland example: import all greenland data and create stack
+g2000 <- im.import("greenland.2000.tif")
+g2005 <- im.import("greenland.2005.tif")
+g2010 <- im.import("greenland.2010.tif")
+g2015 <- im.import("greenland.2015.tif")
+sg <- c(g2000, g2005, g2010, g2015)
+
+gr <-im.import("greenland")
+
+im.multiframe(1,2)
+plot(gr[[1]])
+plot(gr[[4]])
+
+library(viridis)
+plot(gr[[1]], col=plasma(100))
+plot(gr[[4]], col=plasma(100))
+
+dif <- gr[[4]] - gr[[1]]
+dev.off()
+plot(dif)
+
+
+
+# ridgeline plots
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
