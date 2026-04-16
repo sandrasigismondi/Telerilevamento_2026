908700724674c9bbd58b50f251229fc1df2b005e
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-04-16 15:35:36 +0200

    Update 06_exam_example.R

--- Code/06_exam_example.R
+++ Code/06_exam_example.R
@@ -1,1 +1,29 @@
+library(terra)
+library(imageRy)
+
+setwd("C:\\Users\\fabis\\Downloads\\Uni\\SGN Magistrale\\Telerilevamento Geo-Eco in R")
+list.files()
+
+richat<- rast("richatstructure_oli_20260306.jpg")
+richat<- flip(richat)
+plot(richat)
+
+
+########
+png("figura.png")
+plot(richat)
+dev.off()
+
+im.multiframe(2,1)
+plot(richat[[1]])
+plot(richat[[2])
+
+png("bande.png")
+im.multiframe(2,1)
+plot(richat[[1]])
+plot(richat[[2])
+dev.off()
+##########          
+
+
 
