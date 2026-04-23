b7c3c308d6facf4e7f0a6d34214eee26e97c5f73
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-04-23 15:43:31 +0200

    Create 08_functions.R

---
+++ Code/08_functions.R
@@ -0,0 +1,30 @@
+# My functions!
+
+somma <- function(x,y) {
+  z=x+y
+  return(z)
+  }
+
+# Excercise: make the function called difference
+differenza <- function(x,y) {
+  z=x-y
+  return(z)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
