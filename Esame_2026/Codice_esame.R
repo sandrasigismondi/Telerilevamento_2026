bd2a7353bbba6a26fa78b01ecd358a3e6c6d64fa
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-07-16 17:48:38 +0200

    Update Codice_esame.R

--- Esame_2026/Codice_esame.R
+++ Esame_2026/Codice_esame.R
@@ -1,9 +1,8 @@
 # Pacchetti
-library(terra)
-library(ggplot2)
-library(viridis)
-library(RStoolbox)
-library(glacieR)
+library(terra)      # Gestione dei raster e shapefile, operazioni spaziali e funzioni di visualizzazione
+library(viridis)    # Palette cromatiche
+library(RStoolbox)  # Classificazione non supervisionata
+library(glacieR)    # Strumenti per l'analisi dei ghiacciai tramite telerilevamento
 
 
 # Cartella di lavoro
@@ -11,24 +10,31 @@ library(glacieR)
 setwd("C:\\Users\\fabis\\Downloads\\Uni\\SGN Magistrale\\Telerilevamento Geo-Eco in R")
 list.files()
 
+
+# vect() importa lo shapefile come oggetto SpatVector
+
 columbia <- vect("columbia_riproiettato.shp")
 
-# Importa e ritaglia
+
+# rast() importa le immagini Sentinel-2 come oggetti SpatRaster
 
 sep2020 <- rast("columbia_september2020.tif")
-sep2020_mask <- prepareGlacier(sep2020, columbia)
-
 sep2021 <- rast("columbia_september2021.tif")
-sep2021_mask <- prepareGlacier(sep2021, columbia)
-
-
 sep2023 <- rast("columbia_september2023.tif")
+
+
+# prepareGlacier() utilizza internamente:
+# - crop(): ritaglia il raster all'estensione dello shapefile
+# - mask()): elimina i pixel esterni al contorno del ghiacciaio
+
+sep2020_mask <- prepareGlacier(sep2020, columbia)
+sep2021_mask <- prepareGlacier(sep2021, columbia)
 sep2023_mask <- prepareGlacier(sep2023, columbia)
 
 
-# Visualizzazione RGB
+par(mfrow = c(1,3)) # Dispone i grafici in una finestra con 1 riga e 3 colonne
 
-par(mfrow = c(1,3))
+# plotglacier() visualizza l'immagine in composizione RGB naturale: r = rosso, g = verde, b = blu
 
 plotGlacierRGB(sep2020_mask, r = 3, g = 2, b = 1, title = "Settembre 2020")
 plotGlacierRGB(sep2021_mask, r = 3, g = 2, b = 1, title = "Settembre 2021")
@@ -36,10 +42,13 @@ plotGlacierRGB(sep2023_mask, r = 3, g = 2, b = 1, title = "Settembre 2023")
 
 
 # NDSI
+# glacierNDSI() calcola l'NDSI con la banda verde e la banda SWIR
 
 ndsi_sep2020 <- glacierNDSI(sep2020_mask, green = 2, swir = 5)
 ndsi_sep2021 <- glacierNDSI(sep2021_mask, green = 2, swir = 5)
 ndsi_sep2023 <- glacierNDSI(sep2023_mask, green = 2, swir = 5)
+
+# plotNDSI() visualizza le mappe NDSI con la palette "mako"
 
 plotNDSI(ndsi_sep2020, title = "NDSI - Settembre 2020")
 plotNDSI(ndsi_sep2021, title = "NDSI - Settembre 2021")
@@ -47,9 +56,13 @@ plotNDSI(ndsi_sep2023, title = "NDSI - Settembre 2023")
 
 
 # NDWI
+# glacierNDWI() calcola l'NDWI con la banda verde e la banda NIR
+
 ndwi_sep2020 <- glacierNDWI(sep2020_mask, green = 2, nir = 4)
 ndwi_sep2021 <- glacierNDWI(sep2021_mask, green = 2, nir = 4)
 ndwi_sep2023 <- glacierNDWI(sep2023_mask, green = 2, nir = 4)
+
+# plotNDWI() visualizza le mappe NDWI con la palette "viridis
 
 plotNDWI(ndwi_sep2020, title = "NDWI - Settembre 2020")
 plotNDWI(ndwi_sep2021, title = "NDWI - Settembre 2021")
@@ -57,10 +70,13 @@ plotNDWI(ndwi_sep2023, title = "NDWI - Settembre 2023")
 
 
 # Classificazione
+# glacierClass()
 
 class_sep2020 <- glacierClass(sep2020_mask, nClasses = 3)
 class_sep2021 <- glacierClass(sep2021_mask, nClasses = 3)
 class_sep2023 <- glacierClass(sep2023_mask, nClasses = 3)
+
+# plotClass() visualizza la classificazione con la palette "cividis"
 
 plotClass(class_sep2020, title = "Settembre 2020")
 plotClass(class_sep2021, title = "Settembre 2021")
@@ -69,65 +85,12 @@ plotClass(class_sep2023, title = "Settembre 2023")
 par(mfrow = c(1,1))
 
 
-#################################################
-# Class percentages
-#################################################
-
-class2020 <- class_sep2020$map
-class2021 <- class_sep2021$map
-class2023 <- class_sep2023$map
-
-freq2020 <- freq(class2020)
-freq2021 <- freq(class2021)
-freq2023 <- freq(class2023)
-
-perc2020 <- freq2020$count/sum(freq2020$count)*100
-perc2021 <- freq2021$count/sum(freq2021$count)*100
-perc2023 <- freq2023$count/sum(freq2023$count)*100
-
-round(perc2020,2)
-round(perc2021,2)
-round(perc2023,2)
-
-
-#################################################
-# Glacier surface
-#################################################
-
-pixelArea2020 <- prod(res(class2020))
-pixelArea2021 <- prod(res(class2021))
-pixelArea2023 <- prod(res(class2023))
-
-surface2020 <- freq2020$count*pixelArea2020
-surface2021 <- freq2021$count*pixelArea2021
-surface2023 <- freq2023$count*pixelArea2023
-
-surface2020
-surface2021
-surface2023
-
-# in km2
-surface2020_km2 <- surface2020/1000000
-surface2021_km2 <- surface2021/1000000
-surface2023_km2 <- surface2023/1000000
-
-
-#################################################
-# Summary table
-#################################################
-
-summaryTable <- data.frame(
-  Class = c("Dark surfaces",
-            "Debris-covered ice",
-            "Snow / Clean ice"),
-  September2020 = round(perc2020,2),
-  September2021 = round(perc2021,2),
-  September2023 = round(perc2023,2)
-)
-
-summaryTable
 
 
 
 
-});
+
+
+
+
+
